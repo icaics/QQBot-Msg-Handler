dfcc6adc89d9d7d51f52ca705d3ce94de5e12234
NaChan <yhg000@126.com>
2018-08-25 09:55:51 +0800

    修改 绑定 QR 端口号

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --name qqbot-gf-kalina -p 443:443 -p 8000:8000 -v /docker/qqbot-gf-kalina/plugins:/qqbot -v /docker/qqbot-gf-kalina/config:/root/.qqbot-tmp/ -d --restart=always qqbot-gf-kalina /qqbot.sh
+docker run --name qqbot-gf-kalina -p 8000:8000 -p 8001:8001 -v /docker/qqbot-gf-kalina/plugins:/qqbot -v /docker/qqbot-gf-kalina/config:/root/.qqbot-tmp/ -d --restart=always qqbot-gf-kalina /qqbot.sh
