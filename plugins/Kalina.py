2518f403e68aedd025131c9cbb138ee853b9edd8
菜酱 <yhg000@126.com>
2018-10-03 14:40:16 +0800

    新增 上线下线提示

--- plugins/Kalina.py
+++ plugins/Kalina.py
@@ -13,9 +13,58 @@ import random
 import time
 
 
+def onStartupComplete(bot):
+
+    """
+    # 启动完成时被调用
+    # bot : QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    """
+
+    group = bot.List('group', Kalina.group_name)[0]
+    bot.SendTo(group, '各位指挥官，格林娜摸鱼回来了，我、格林娜、上班！')
+
+
+def onExit(bot, code, reason, error):
+
+    """
+    # MainLoop（主循环）终止时被调用， Mainloop 是一个无限循环，QQBot 登录成功后便开始运
+    # 行，当且仅当以下事件发生时 Mainloop 终止：
+    #     1） 调用了 bot.Stop() ，此时：
+    #         code = 0, reason = 'stop', error = None
+    #     2） 调用了 bot.Restart() ，此时：
+    #         code = 201, reason = 'restart', error = None
+    #     3） 调用了 bot.FreshRestart() ，此时：
+    #         code = 202, reason = 'fresh-restart', error = None
+    #     4） 调用了 sys.exit(x) （ x 不等于 0,201,202,203 ），此时：
+    #         code = x, reason = 'system-exit', error = None
+    #     5） 登录的 cookie 已过期，此时：
+    #         code = 203, reason = 'login-expire', error = None
+    #     6） 发生未知错误 e （暂未出现过，出现则表明 qqbot 程序内部可能存在错误），此时：
+    #         code = 1, reason = 'unknown-error', error = e
+    #
+    # 一般情况下：
+    #     发生 1/2/3/4 时，可以安全的调用 bot.List/SendTo/GroupXXX 等接口
+    #     发生 5/6 时，调用 bot.List/SendTo/GroupXXX 等接口将出错
+    #
+    # 一般情况下，用户插件内的代码和运行错误会被捕捉并忽略，不会引起 MainLoop 的退出
+    #
+    # 本函数被调用后，会执行 sys.exit(code) 退出本次进程并返回到父进程，父进程会根据
+    # “ code 的数值” 以及 “是否配置为自动重启模式” 来决定是否重启 QQBot 。
+    """
+
+    group = bot.List('group', Kalina.group_name)[0]
+    bot.SendTo(group, '各位指挥官，格林娜去摸鱼了，告辞.png')
+
+
 def onQQMessage(bot, contact, member, content):
 
-    """ QQBot 全局调用入口 """
+    """
+    # 当收到 QQ 消息时被调用
+    # bot     : QQBot 对象，提供 List/SendTo/GroupXXX/Stop/Restart 等接口，详见文档第五节
+    # contact : QContact 对象，消息的发送者
+    # member  : QContact 对象，仅当本消息为 群或讨论组 消息时有效，代表实际发消息的成员
+    # content : str 对象，消息内容
+    """
 
     # print(bot)
     # print(contact)
